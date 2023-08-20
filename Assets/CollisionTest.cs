46624379781791c1c4d3a2823d9ae63aa06a075e
CrispySnack <koen-de-vos@hotmail.com>
2023-08-20 22:01:55 +0200

    collision and messages

---
+++ Assets/CollisionTest.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class CollisionTest : MonoBehaviour
+{
+    public GameObject goodmssg;
+    public GameObject wrongmssg;
+    public bool IsColliding = false;
+    public float coltimer = 3;
+    public bool IsCorrect = false;
+    public bool IsWrong = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay(Collider other)
+    {
+            if(coltimer > 0)
+                {
+                    coltimer -= Time.deltaTime;
+                }
+        
+            else
+                {
+                    coltimer = 0;
+
+                    if(other.tag == "GFT")
+                    {
+                        IsCorrect = true;
+                    }
+                    else if(other.tag == "PMD")
+                    {
+                        IsWrong = true;
+                    }
+                    else if(other.tag == "Papier")
+                    {
+                        IsWrong = true;
+                    }
+                    else if(other.tag == "Rest")
+                    {
+                        IsWrong = true;
+                    }
+                    else if(other.tag == "Inlever")
+                    {
+                        IsWrong = true;
+                    }
+                }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        coltimer = 3;
+        IsCorrect = false;
+        IsWrong = false;
+
+    }
+
+    void Update()
+    {
+        // Timer for correct/wrong
+        /* if(IsColliding == true)
+        {
+            if(coltimer > 0)
+                {
+                    coltimer -= Time.deltaTime;
+                }
+        
+            else
+                {
+                    coltimer = 0;
+                    IsCorrect = true;
+                }
+        }
+        */
+
+        if(IsCorrect == true)
+        {
+            Instantiate(goodmssg, new Vector3(0,0,0), Quaternion.identity);
+        }
+
+        if(IsWrong == true)
+        {
+            Instantiate(wrongmssg, new Vector3(0,0,0), Quaternion.identity);
+        }
+    }
+
+}
